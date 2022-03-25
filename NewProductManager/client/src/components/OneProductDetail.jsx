2968a93c3f84d058ed9701bada7f02a9b444126a
Brandon_Hart <sugb73@gmail.com>
2022-03-25 01:15:20 -0400

    Completed Assignments

--- NewProductManager/client/src/components/OneProductDetail.jsx
+++ NewProductManager/client/src/components/OneProductDetail.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router";
 import axios from 'axios'
 import { useHistory } from "react-router-dom"
 
-// Adding another git commit
 
 const OneProductDetail = () => {
 
